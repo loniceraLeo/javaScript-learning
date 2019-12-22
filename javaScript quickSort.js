202a781dd38f735cd75f7d840bf2e1808d05ff41
loniceraLeo <57742903+loniceraLeo@users.noreply.github.com>
2019-12-22 13:30:30 +0800

    Add files via upload

---
+++ javaScript quickSort.js
@@ -0,0 +1,29 @@
+var arr=[2,25,4,24,5,5,32,24,5,13,96];
+function quickSort(arr,left,right)
+{
+	if (left>=right) return;
+	var leftPtr=left,
+		rightPtr=right+1,
+		currentNode=arr[leftPtr];
+	while(true)
+	{
+		do{
+			++leftPtr;
+			}while(arr[leftPtr]<currentNode);
+		do{
+			--rightPtr;
+		}while(arr[rightPtr]>currentNode);
+		if (leftPtr>=rightPtr) break;
+		var t=arr[leftPtr];
+		arr[leftPtr]=arr[rightPtr];
+		arr[rightPtr]=t;
+	}
+	var t2=arr[left];
+	arr[left]=arr[rightPtr];
+	arr[rightPtr]=t2;
+	quickSort(arr,left,rightPtr-1);
+	quickSort(arr,rightPtr+1,right);
+}
+    quickSort(arr,0,10);
+	for(var i in arr)
+		console.log(arr[i]);
